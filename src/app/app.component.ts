3a60ed31f4a1f9135a53eeacf591ec1c2cca6f70
dammaladinshpaul21 <dammaladineshpaul21@gmail.com>
2023-04-26 15:30:53 +0530

    Update add data and remove data code in the app.compount.ts file and make changes in butting in Html and added event's too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks=[
+    "Kiran Data",
+  ]
+  
+  add(newTask:string){
+    this.tasks.push(newTask)
+  }
+
+  remove(exisitingTask: string){
+    var userConfirm = confirm(`Are you Do you want to remove the task \n "${exisitingTask}"`)
+    if (userConfirm){
+      this.tasks = this.tasks.filter(task=> task != exisitingTask)
+    }
+  }
 }
